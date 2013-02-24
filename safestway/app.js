1e9d442c2bd48c2f0c9fa7f7143f172127a7ede6
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2013-02-24 10:31:55 -0600

    Add node.js with twitter boostrap, and file describing its setup.

---
+++ safestway/app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+app.use(express.compiler({ src : __dirname + '/public', enable: ['less']}));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Compatible
+
+// Now less files with @import 'whatever.less' will work(https://github.com/senchalabs/connect/pull/174)
+var TWITTER_BOOTSTRAP_PATH = './vendor/twitter/bootstrap/less';
+express.compiler.compilers.less.compile = function(str, fn){
+  try {
+    var less = require('less');var parser = new less.Parser({paths: [TWITTER_BOOTSTRAP_PATH]});
+    parser.parse(str, function(err, root){fn(err, root.toCSS());});
+  } catch (err) {fn(err);}
+}
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
